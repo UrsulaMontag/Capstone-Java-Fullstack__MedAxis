b53a29f77038e555ee7fcf18fb8476ba94d76407
ursulamontag <montagu666@gmail.com>
2024-07-19 10:39:43 +0200

    Add form to add patient

--- frontend/src/components/PatientForm.tsx
+++ frontend/src/components/PatientForm.tsx
@@ -1,6 +1,6 @@
 import usePatientStore from "../stores/usePatientStore.ts";
 import {PatientToAdd} from "../models/Patient.ts";
-import {FormEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 
 export default function PatientForm() {
     const createPatient: (newPatient: PatientToAdd) => void = usePatientStore((state) => state.createPatient);
@@ -15,11 +15,25 @@ export default function PatientForm() {
         event.preventDefault();
         createPatient(patientInput);
         setPatientInput(initialInputState);
+        alert("")
     }
 
     return (
         <form onSubmit={(event: FormEvent<HTMLFormElement>) => handleSubmit(event)}>
-
+            <label>Firstname: </label>
+            <input type="text" value={patientInput.firstname} onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                setPatientInput({...patientInput, firstname: event.target.value});
+            }}/>
+            <label>Lastname: </label>
+            <input type="text" value={patientInput.lastname} onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                setPatientInput({...patientInput, lastname: event.target.value});
+            }}/>
+            <label>Date of Birth: </label>
+            <input type="text" value={patientInput.dateOfBirth} onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                setPatientInput({...patientInput, dateOfBirth: event.target.value});
+            }}/>
+            <button>Register</button>
+            <button>Cancel</button>
         </form>
     )
 }
--- frontend/src/routes/CreatePatient.tsx
+++ frontend/src/routes/CreatePatient.tsx
@@ -1,6 +1,8 @@
+import PatientForm from "../components/PatientForm.tsx";
+
 export default function CreatePatient() {
 
     return (
-        <></>
+        <PatientForm/>
     )
 }
