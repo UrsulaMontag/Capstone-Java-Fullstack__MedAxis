221b83c641040a8cccc5c65eb3f2549d92816084
ursulamontag <montagu666@gmail.com>
2024-07-22 12:11:00 +0200

    Add controller and service tests for get routes

--- backend/src/test/java/um_backend/controller/PatientControllerTest.java
+++ backend/src/test/java/um_backend/controller/PatientControllerTest.java
@@ -9,6 +9,11 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import um_backend.models.Patient;
+import um_backend.repository.PatientRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,25 +23,74 @@ class PatientControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private PatientRepository patientRepository;
+
     @Test
-    void createPatient_returnsNewPatient_withRandomId() {
-        try {
-            mockMvc.perform(MockMvcRequestBuilders.post("/api/patients/add")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content("""
-                                    {"firstname": "Max",
-                                     "lastname": "Mustermann",
-                                     "dateOfBirth": "1984-02-25"}
-                                    """))
-                    .andExpect(MockMvcResultMatchers.status().isCreated())
-                    .andExpect(MockMvcResultMatchers.content().json("""
-                            {"firstname": "Max",
-                             "lastname": "Mustermann",
-                             "dateOfBirth": "1984-02-25"}
-                            """))
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    void getAllPatients_returnsListOfAllRegisteredPatients() throws Exception {
+        patientRepository.saveAll(List.of(
+                (new Patient("1", "Max", "Mustermann", LocalDate.of(2001, 4, 12))),
+                (new Patient("2", "Erika", "Musterfrau", LocalDate.of(1986, 5, 4))),
+                (new Patient("3", "Gerlinde", "Häberle", LocalDate.of(1998, 4, 16)))
+        ));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/patients"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                            [
+                                {
+                                "id": "1",
+                                "firstname": "Max",
+                                "lastname": "Mustermann",
+                                "dateOfBirth": "2001-04-12"
+                                },
+                                {
+                                "id": "2",
+                                "firstname": "Erika",
+                                "lastname": "Musterfrau",
+                                "dateOfBirth": "1986-05-04"
+                                },
+                                {
+                                "id": "3",
+                                "firstname": "Gerlinde",
+                                "lastname": "Häberle",
+                                "dateOfBirth": "1998-04-16"
+                                }
+                            ]
+                        """));
+    }
+
+    @Test
+    void getPatientById_returnsPatient_foundByGivenId() throws Exception {
+        patientRepository.saveAll(List.of(
+                (new Patient("2", "Erika", "Musterfrau", LocalDate.of(1986, 5, 4)))
+        ));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/patients/2"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "id": "2",
+                            "firstname": "Erika",
+                            "lastname": "Musterfrau",
+                            "dateOfBirth": "1986-05-04"
+                        }
+                        """));
+    }
+
+    @Test
+    void createPatient_returnsNewPatient_withRandomId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/patients/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"firstname": "Max",
+                                 "lastname": "Mustermann",
+                                 "dateOfBirth": "1984-02-25"}
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {"firstname": "Max",
+                         "lastname": "Mustermann",
+                         "dateOfBirth": "1984-02-25"}
+                        """))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
     }
 }
--- backend/src/test/java/um_backend/services/PatientServiceTest.java
+++ backend/src/test/java/um_backend/services/PatientServiceTest.java
@@ -3,6 +3,7 @@ package um_backend.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.DirtiesContext;
+import um_backend.exeptions.InvalidIdException;
 import um_backend.models.Patient;
 import um_backend.models.dto.PatientPostDto;
 import um_backend.repository.PatientRepository;
@@ -10,9 +11,9 @@ import um_backend.repository.PatientRepository;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -32,6 +33,42 @@ class PatientServiceTest {
             add(new Patient("2", "Erika", "Musterfrau", LocalDate.of(1986, 5, 4)));
             add(new Patient("3", "Gerlinde", "Häberle", LocalDate.of(1998, 4, 16)));
         }};
+    }
+
+    @Test
+    void getAllPatients_returnsAllRegisteredPatients() {
+        when(mockPatientRepository.findAll()).thenReturn(testPatientList);
+        List<Patient> actual = patientService.getAllPatients();
+        verify(mockPatientRepository).findAll();
+        assertEquals(testPatientList, actual);
+    }
+
+    @Test
+    void getAllPatients_throwsException_whenWentWrong() {
+        when(mockPatientRepository.findAll()).thenThrow(new RuntimeException("Error getting all patients"));
+        try {
+            patientService.getAllPatients();
+            verify(mockPatientRepository).findAll();
+            verify(patientService).getAllPatients();
+            fail("Expected exception, but was not thrown");
+        } catch (Exception e) {
+            assertEquals("Error getting all patients", e.getMessage());
+        }
+    }
+
+    @Test
+    void getPatientById_returnsPatient_withGivenId() throws InvalidIdException {
+        when(mockPatientRepository.findById("2")).thenReturn(Optional.of(testPatientList.get(1)));
+        Patient actual = patientService.getPatientById("2");
+        verify(mockPatientRepository).findById("2");
+        assertEquals(testPatientList.get(1), actual);
+    }
+
+    @Test
+    void getPatientById_throwsException_whenPatientNotFound() {
+        when(mockPatientRepository.findById("2")).thenReturn(Optional.empty());
+        assertThrows(InvalidIdException.class, () -> patientService.getPatientById("2"));
+        verify(mockPatientRepository).findById("2");
     }
 
     @Test
@@ -59,7 +96,7 @@ class PatientServiceTest {
             patientService.createPatient(newPatient);
             verify(mockPatientRepository).save(any(Patient.class));
             fail("Expected exception, but was not thrown");
-        } catch (NullPointerException e) {
+        } catch (RuntimeException e) {
             assertEquals("Error message", e.getMessage());
         }
     }
