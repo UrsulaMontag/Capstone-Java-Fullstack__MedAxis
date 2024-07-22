d0a8252f67a134801aaab83f6eb771b629cceb0a
ursulamontag <montagu666@gmail.com>
2024-07-22 09:57:51 +0200

    Add controller and service logic for get routes

--- backend/src/main/java/um_backend/controller/PatientController.java
+++ backend/src/main/java/um_backend/controller/PatientController.java
@@ -3,9 +3,12 @@ package um_backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import um_backend.exeptions.InvalidIdException;
 import um_backend.models.Patient;
 import um_backend.models.dto.PatientPostDto;
 import um_backend.services.PatientService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
@@ -13,6 +16,18 @@ import um_backend.services.PatientService;
 public class PatientController {
 
     private final PatientService patientService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    public Patient getPatientById(@PathVariable String id) throws InvalidIdException {
+        return patientService.getPatientById(id);
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/add")
--- backend/src/main/java/um_backend/services/PatientService.java
+++ backend/src/main/java/um_backend/services/PatientService.java
@@ -2,9 +2,12 @@ package um_backend.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import um_backend.exeptions.InvalidIdException;
 import um_backend.models.Patient;
 import um_backend.models.dto.PatientPostDto;
 import um_backend.repository.PatientRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +15,14 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
     private final UtilService utilService;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient getPatientById(String id) throws InvalidIdException {
+        return patientRepository.findById(id).orElseThrow(() -> new InvalidIdException("Patient with id " + id + "not found!"));
+    }
 
     public Patient createPatient(PatientPostDto patient) {
         Patient newPatient = new Patient(utilService.generateId(), patient.firstname(), patient.lastname(), patient.dateOfBirth());
