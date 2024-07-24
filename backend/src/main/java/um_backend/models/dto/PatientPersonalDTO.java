14cf03400a250eb83f3b3406ab6e4f7b7b13c830
ursulamontag <montagu666@gmail.com>
2024-07-24 09:43:30 +0200

    Fix security issue sonarcloud

--- backend/src/main/java/um_backend/models/dto/PatientPersonalDTO.java
+++ backend/src/main/java/um_backend/models/dto/PatientPersonalDTO.java
@@ -1,12 +1,10 @@
 package um_backend.models.dto;
 
 import lombok.With;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 
 @With
-@Document("patients")
 public record PatientPersonalDTO(
         String firstname,
         String lastname,
