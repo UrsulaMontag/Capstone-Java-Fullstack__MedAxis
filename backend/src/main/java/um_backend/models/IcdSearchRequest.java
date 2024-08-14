d5072e902d99a339fd6543158584e9b4d072491e
ursulamontag <montagu666@gmail.com>
2024-08-14 14:59:02 +0200

    Fix f***** backend endpoints to get ect run

--- backend/src/main/java/um_backend/models/IcdSearchRequest.java
+++
@@ -1,6 +0,0 @@
-package um_backend.models;
-
-public record IcdSearchRequest(String q, String chapterFilter, String subtreeFilter, String includePostcoordination,
-                               String useBroaderSynonyms, String useFlexiSearch, String includeKeywordResult,
-                               String flatResults, String highlightingEnabled, String medicalCodingMode) {
-}
