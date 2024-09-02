a7083fce39baca6bcde183fc0f7e583487f917af
ursulamontag <montagu666@gmail.com>
2024-09-02 11:49:27 +0200

    Adjust patient form to expanded model

--- frontend/src/components/patient/PatientForm.tsx
+++ frontend/src/components/patient/PatientForm.tsx
@@ -22,6 +22,16 @@ export default function PatientForm() {
         firstname: currentPatient ? currentPatient.firstname : "",
         lastname: currentPatient ? currentPatient.lastname : "",
         dateOfBirth: currentPatient ? currentPatient.dateOfBirth : "",
+        gender: currentPatient ? currentPatient.gender : "",
+        nationality: currentPatient ? currentPatient.nationality : "",
+        maritalStatus: currentPatient ? currentPatient.maritalStatus : "",
+        primaryLanguage: currentPatient ? currentPatient.primaryLanguage : "",
+        occupation: currentPatient ? currentPatient.occupation : "",
+        emergencyContact: currentPatient ? currentPatient.emergencyContact : {
+            name: "",
+            relationship: "",
+            phoneNumber: ""
+        },
         insuranceNr: currentPatient ? currentPatient.insuranceNr : "",
         contactInformation: currentPatient ? currentPatient.contactInformation : {
             phoneNr: "",
@@ -40,28 +50,95 @@ export default function PatientForm() {
         const newErrors: { [key: string]: string } = {};
         let isValid = true;
 
-        if (!validation.isValidName(patientInput.firstname)) {
-            newErrors.firstname = "Invalid first name format.";
-            isValid = false;
-        }
-        if (!validation.isValidName(patientInput.lastname)) {
-            newErrors.lastname = "Invalid last name format.";
-            isValid = false;
-        }
-        if (!validation.isValidDateOfBirth(patientInput.dateOfBirth)) {
-            newErrors.dateOfBirth = "Invalid date of birth.";
-            isValid = false;
-        }
-        if (!validation.isValidInsuranceNumber(patientInput.insuranceNr)) {
-            newErrors.insuranceNr = "Invalid insurance number format.";
-            isValid = false;
-        }
-        if (patientInput.contactInformation.phoneNr && !validation.isValidPhoneNumber(patientInput.contactInformation.phoneNr)) {
-            newErrors.phone = "Invalid phoneNr number format.";
-            isValid = false;
-        }
-        if (patientInput.contactInformation.email && !validation.isValidEmail(patientInput.contactInformation.email)) {
-            newErrors.email = "Invalid email address format.";
+        const validations = [
+            {
+                field: 'firstname',
+                method: validation.isValidName,
+                validWhen: true,
+                message: 'Invalid first name format.',
+                value: patientInput.firstname
+            },
+            {
+                field: 'lastname',
+                method: validation.isValidName,
+                validWhen: true,
+                message: 'Invalid last name format.',
+                value: patientInput.lastname
+            },
+            {
+                field: 'dateOfBirth',
+                method: validation.isValidDateOfBirth,
+                validWhen: true,
+                message: 'Invalid date of birth.',
+                value: patientInput.dateOfBirth
+            },
+            {
+                field: 'gender',
+                method: validation.isValidGender,
+                validWhen: true,
+                message: 'Invalid gender format.',
+                value: patientInput.gender
+            },
+            {
+                field: 'nationality',
+                method: validation.isValidNationality,
+                validWhen: true,
+                message: 'Invalid nationality format.',
+                value: patientInput.nationality
+            },
+            {
+                field: 'maritalStatus',
+                method: validation.isValidMaritalStatus,
+                validWhen: true,
+                message: 'Invalid marital format.',
+                value: patientInput.maritalStatus
+            },
+            {
+                field: 'primaryLanguage',
+                method: validation.isValidPrimaryLanguage,
+                validWhen: true,
+                message: 'Invalid language format.',
+                value: patientInput.primaryLanguage
+            },
+            {
+                field: 'occupation',
+                method: validation.isValidOccupation,
+                validWhen: true,
+                message: 'Invalid occupation format.',
+                value: patientInput.occupation
+            },
+            {
+                field: 'insuranceNr',
+                method: validation.isValidInsuranceNumber,
+                validWhen: true,
+                message: 'Invalid insurance number format.',
+                value: patientInput.insuranceNr
+            },
+            {
+                field: 'phoneNr',
+                method: validation.isValidPhoneNumber,
+                validWhen: patientInput.contactInformation.phoneNr !== undefined,
+                message: 'Invalid phone number format.',
+                value: patientInput.contactInformation.phoneNr
+            },
+            {
+                field: 'email',
+                method: validation.isValidEmail,
+                validWhen: patientInput.contactInformation.email !== undefined,
+                message: 'Invalid email address format.',
+                value: patientInput.contactInformation.email
+            }
+        ];
+
+        validations.forEach(validation => {
+            if (validation.value !== undefined && validation.method(validation.value) !== validation.validWhen) {
+                newErrors[validation.field] = validation.message;
+                isValid = false;
+            }
+        });
+
+        if (!validation.isValidEmergencyContact(patientInput.emergencyContact)) {
+            newErrors.emergencyContact = 'Invalid emergency contact format.';
             isValid = false;
         }
         setErrors(newErrors);
@@ -92,10 +169,35 @@ export default function PatientForm() {
         const {name, value} = event.target;
         setPatientInput((prevState) => ({
             ...prevState,
+            [name]: value
+        }));
+    };
+    const handleContactInformationChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setPatientInput((prevState) => ({
+            ...prevState,
             contactInformation: {
                 ...prevState.contactInformation,
-                [name]: value
-            }
+                [name]: value,
+            },
+        }));
+    };
+
+    const handleEmergencyContactChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setPatientInput((prevState) => ({
+            ...prevState,
+            emergencyContact: {
+                ...prevState.emergencyContact,
+                [name]: value,
+            },
+        }));
+    };
+    const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        const {name, value} = event.target;
+        setPatientInput((prevState) => ({
+            ...prevState,
+            [name]: value,
         }));
     };
 
@@ -126,6 +228,16 @@ export default function PatientForm() {
             <br/>
             {errors.dateOfBirth && <Typography variant="error-info">{errors.dateOfBirth}</Typography>}
 
+            <label>Gender:</label>
+            <select name="gender" id="gender-select" value={patientInput.gender} onChange={handleSelectChange}>
+                <option value="">--Please choose gender--</option>
+                <option value="female">Female</option>
+                <option value="male">Male</option>
+                <option value="other">Other</option>
+            </select>
+            <br/>
+            {errors.gender && <Typography variant="error-info">{errors.gender}</Typography>}
+
             <label>Insurance Nr:</label>
             <input required type="text" value={patientInput.insuranceNr}
                    onChange={(event: ChangeEvent<HTMLInputElement>) => {
@@ -139,7 +251,7 @@ export default function PatientForm() {
                 type="text"
                 name="phoneNr"
                 value={patientInput.contactInformation.phoneNr}
-                onChange={handleInputChange}
+                onChange={handleContactInformationChange}
             />
             <br/>
             {errors.phone && <Typography variant="error-info">{errors.phone}</Typography>}
@@ -149,7 +261,7 @@ export default function PatientForm() {
                 type="email"
                 name="email"
                 value={patientInput.contactInformation.email}
-                onChange={handleInputChange}
+                onChange={handleContactInformationChange}
             />
             <br/>
             {errors.email && <Typography variant="error-info">{errors.email}</Typography>}
@@ -159,7 +271,7 @@ export default function PatientForm() {
                 type="text"
                 name="address"
                 value={patientInput.contactInformation.address}
-                onChange={handleInputChange}
+                onChange={handleContactInformationChange}
             />
 
             <label>Town:</label>
@@ -167,9 +279,77 @@ export default function PatientForm() {
                 type="text"
                 name="town"
                 value={patientInput.contactInformation.town}
-                onChange={handleInputChange}
+                onChange={handleContactInformationChange}
             />
 
+            <label>Emergency Contact:</label>
+            <br/>
+            <label>Name:</label>
+            <input
+                type="text"
+                name="name"
+                value={patientInput.emergencyContact.name}
+                onChange={handleEmergencyContactChange}
+            />
+
+            <label>Relationship:</label>
+            <input
+                type="text"
+                name="relationship"
+                value={patientInput.emergencyContact.relationship}
+                onChange={handleEmergencyContactChange}
+            />
+            <label>Phone:</label>
+            <input
+                type="text"
+                name="phoneNumber"
+                value={patientInput.emergencyContact.phoneNumber}
+                onChange={handleEmergencyContactChange}
+            />
+            <br/>
+            {errors.emergencyContact && <Typography variant="error-info">{errors.emergencyContact}</Typography>}
+
+            <label>Nationality:</label>
+            <input
+                type="text"
+                name="nationality"
+                value={patientInput.nationality}
+                onChange={handleInputChange}
+            />
+            <br/>
+            {errors.nationality && <Typography variant="error-info">{errors.nationality}</Typography>}
+
+            <label>Marital Status:</label>
+            <select name="maritalStatus" id="marital-status-select" value={patientInput.maritalStatus}
+                    onChange={handleSelectChange}>
+                <option value="">--Please choose marital status--</option>
+                <option value="single">Single</option>
+                <option value="married">Married</option>
+                <option value="divorced">Divorced</option>
+                <option value="widowed">Widowed</option>
+            </select>
+            <br/>
+            {errors.maritalStatus && <Typography variant="error-info">{errors.maritalStatus}</Typography>}
+
+            <label>Primary Language:</label>
+            <input
+                type="text"
+                name="primaryLanguage"
+                value={patientInput.primaryLanguage}
+                onChange={handleInputChange}
+            />
+            <br/>
+            {errors.primaryLanguage && <Typography variant="error-info">{errors.primaryLanguage}</Typography>}
+
+            <label>Occupation:</label>
+            <input
+                type="text"
+                name="occupation"
+                value={patientInput.occupation}
+                onChange={handleInputChange}
+            />
+            <br/>
+            {errors.primaryLanguage && <Typography variant="error-info">{errors.primaryLanguage}</Typography>}
 
             <div>
                 <Button variant="ok"><img alt="ok-button save"
