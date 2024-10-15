b490e2d56797e5a8fecc503590228c74efed12c0
ursulamontag <montagu666@gmail.com>
2024-10-15 11:08:46 +0200

    Change architecture of api ect usage in frontend

---
+++ frontend/src/components/icd_api/IcdECTWrapper.tsx
@@ -0,0 +1,22 @@
+import {ISelectedEntity} from "../../models/icd_api/icdECTSelectedEntity.ts";
+import IcdECT from "./IcdECT"; // Import the IcdECT component
+
+type IcdECTWrapperProps = {
+    onSelect: (icdCode: { code: string; description: string }) => void;
+}
+
+export default function IcdECTWrapper(props: Readonly<IcdECTWrapperProps>) {
+    const handleSelectedEntity = (selectedEntity: ISelectedEntity) => {
+        const code = selectedEntity.code;
+        const title = selectedEntity.selectedText;
+
+        props.onSelect({code, description: title});
+    };
+
+    return (
+        <div>
+            <IcdECT onEntitySelect={handleSelectedEntity}/>
+        </div>
+    );
+}
+
